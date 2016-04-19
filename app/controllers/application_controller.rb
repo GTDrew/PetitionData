12e5051df80e27af6908af00c036031b86f8d176
Drew Reynolds <drew.reynolds@gramercytech.com>
2016-04-19 17:33:02 -0400

    Show petition signers table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def nyack_signer_count
-    nyack_signers ||= Signer.where(:location => 'Nyack, NY').count
+    nyack_signer_count ||= Signer.where(:location => 'Nyack, NY').count
+  end
+
+  def nyack_signers
+    nyack_signers ||= Signer.where(:location => 'Nyack, NY')
   end
 
   def nyack_percentage
     nyack_percentage ||= ((nyack_signer_count.to_f/6898.00)*100).round(2)
   end
 
-  # Client.connection.select_all("SELECT first_name, created_at FROM clients WHERE id = '1'")
-
-  # def all_future_events
-  # all_future_events ||= Event.where('start_time >= ?', Date.today)
-  # end
-
   helper_method :nyack_signer_count
   helper_method :nyack_percentage
+  helper_method :nyack_signers
 end
